3fe56299d0c40a35be3792dbf49c0b7871e70d6d
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-09 13:24:05 -0400

    initial commit

--- src/operator.java
+++ src/operator.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class operator {
+public class operator {
+    public static void main(String[] args) {
+        double myFirstValue = 20.00;
+        double mySecondValue = 80.00;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00;
+        System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if(!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
 }
